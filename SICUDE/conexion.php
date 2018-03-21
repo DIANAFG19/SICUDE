6481cb33ed2773be6dcdde621430d041f6e69fb5
DIANAFG19 <diana1310fg@gmail.com>
2018-03-20 21:47:59 -0600

    Ya tenemos lo de Registro.
    Ya tenemos lo de Registro.

--- SICUDE/conexion.php
+++ SICUDE/conexion.php
@@ -1,8 +1,64 @@
-
-<?php
-
-$conexión = new MongoDB(, 'ds012345.mlab.com:56789/sicude'); // concectar a un host remoto en un puerto dado
-
-echo "Conexion hecha" + conexion;
-
-?>
+<html><head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- PAGE settings -->
+  <link rel="icon" href="Imagenes/icono.ico">
+  <title>SICUDE</title>
+  <!-- CSS dependencies -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="theme.css" type="text/css">
+  <!-- Script: Make my navbar transparent when the document is scrolled to top -->
+  <script src="https://d33wubrfki0l68.cloudfront.net/js/12e3ca424c3c55e3d306e6423ed9e81f260dd657/aquamarine/js/navbar-ontop.js"></script>
+  <!-- Script: Animated entrance -->
+  <script src="https://d33wubrfki0l68.cloudfront.net/js/fe6311b3c294cba469a3939f21603640522c41e5/aquamarine/js/animate-in.js"></script>
+</head><body>
+  <!-- Navbar -->
+  <nav class="navbar-expand-md navbar-primary navbar fixed-top bg-pr">
+    <div class="container">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar3SupportedContent" aria-controls="navbar3SupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+      <div class="collapse navbar-collapse text-center justify-content-center" id="navbar3SupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item mx-3">
+            <a class="nav-link text-light"><b class="text-light text-center w-1000"><b class="border-light"><h1><b>UNITEC</b></h1>
+              </b>
+              </b>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Cover -->
+  <div class="align-items-center d-flex photo-overlay py-5 cover" style="background-image: url(&quot;Imagenes/Fondo.jpg&quot;);">
+    <div class="container">
+        <div class="col-lg-6 p-3 mx-auto">
+          <form class="p-5 bg-dark-opaque" method="post" action="conexion.php">
+            <h4 class="mb-4 text-center">Ingresa Aquí</h4>
+            <div class="form-group"> <label>Nombre de Usuario</label>
+              <input class="form-control" placeholder=""> </div>
+            <div class="form-group"> <label>Contraseña</label>
+              <input class="form-control" placeholder=""> </div>
+            <button type="submit" class="btn mt-4 btn-block btn-warning p-2" class="text-dark">Iniciar sesión</b></button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="text-rigth bg-primary">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 p-4 bg-primary text-primary">
+          <h5> Página hecha por los estudiantes de UNITEC Campus Ecatepec 18-2.&nbsp;
+            <br>Creadores:
+            <br>Flores García Diana Laura			E-mail: diana1310fg@gmail.com
+            <br>Hinojosa Jiménez María Magdalena	E-mail:
+            <br>Herrera Equihua Iván 				E-mail:
+          </h5>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- JavaScript dependencies -->
+  <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+  
+</body></html>
