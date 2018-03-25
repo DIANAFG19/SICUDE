8257ddc2b7fcc1bd06c6637f7b60bdcdf68c0730
Diana Flores García <diana_1310fg@hotmail.com>
2018-03-25 16:32:07 -0600

    Ya está concetado a la Base de Datos y funciona correctamente.
    Ya está concetado a la Base de Datos y funciona correctamente.

---
+++ SICUDE/login.php
@@ -0,0 +1,27 @@
+<?php
+$matricula=$_POST['matricula'];
+$pass=$_POST['pswd'];
+$db_host="198.91.81.6";
+$db_nombre="sicudex1_sistema";
+$db_usuario="sicudex1_diana";
+$db_pass="123456";
+$conexion=mysqli_connect($db_host,$db_usuario,$db_pass,$db_nombre);
+if(mysqli_connect_errno($db_nombre)){
+  echo mysqli_error($conexion);
+}
+else{
+  $consulta="SELECT matricula FROM usuario WHERE matricula LIKE '$matricula' AND password LIKE '$pass'" ;
+  $resultado=mysqli_num_rows(mysqli_query($conexion,$consulta));
+  if($resultado==0){
+    echo "No existe el usuario o no coincide con la contraseña ingresada";
+  }
+  else{
+    echo "Bienvenido Cliente";
+    /*$respuesta=mysqli_query($conexion,$consulta);
+    while($rows=mysqli_fetch_array($respuesta)){
+    echo "<br>ID PERSONA: ".$rows[0]."<br>";
+    include("compras.html");*/
+  }
+}
+mysqli_close($conexion);
+?>
