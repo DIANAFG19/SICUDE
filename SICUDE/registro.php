5423db0d17ff4fe068abbf358fbc1207c02257a3
Diana Flores García <diana_1310fg@hotmail.com>
2018-03-25 21:25:34 -0600

    Ya funciona el dar de alta a un usuario y el ingresar a la página como alumno.
    Ya funciona el dar de alta a un usuario y el ingresar a la página como alumno.

--- SICUDE/registro.php
+++ SICUDE/registro.php
@@ -6,33 +6,26 @@ $correo=$_POST['correo'];
 $pass=$_POST['pswd'];
 $rpass=$_POST['pswdc'];
 
-if(isset($_POST['terminos'])
-{
-	if($pass==$rpass){
-		$db_host="198.91.81.6";
-		$db_nombre="sicudex1_sistema";
-		$db_usuario="sicudex1_diana";
-		$db_pass="123456";
-		$conexion=mysqli_connect($db_host,$db_usuario,$db_pass,$db_nombre);
-		if(mysqli_connect_errno($db_nombre)){
-			echo mysqli_error($conexion);
-		}
-		else{
-			echo 'USTED HA SIDO REGISTRADO CORRECTAMENTE';
-			$registrar="INSERT INTO usuario VALUES('$matricula','$nombre','$telefono','$correo','$pass')";
-			mysqli_query($conexion,$registrar);
-			include("index.html");
-		}
-		mysqli_close($conexion);
+if($pass==$rpass){
+	$db_host="198.91.81.6";
+	$db_nombre="sicudex1_sistema";
+	$db_usuario="sicudex1_diana";
+	$db_pass="123456";
+	$conexion=mysqli_connect($db_host,$db_usuario,$db_pass,$db_nombre);
+	if(mysqli_connect_errno($db_nombre)){
+		echo mysqli_error($conexion);
 	}
 	else{
-		echo "Las contraseñas no coinciden, inténtelo de nuevo.";
-		include("registro.html");
+		echo 'USTED HA SIDO REGISTRADO CORRECTAMENTE';
+		$registrar="INSERT INTO usuario VALUES('$matricula','$nombre','$telefono','$correo','$pass')";
+		mysqli_query($conexion,$registrar);
+		include("index.html");
 	}
+	mysqli_close($conexion);
 }
-else
-{
-	echo 'Necesita aceptar terminos y condiciones.';
+else{
+	echo "Las contraseñas no coinciden, inténtelo de nuevo.";
+	include("registro.html");
 }
 
 ?>
