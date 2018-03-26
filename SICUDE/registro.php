a7ff595020578b40b8dfebb075de1e40ceb69b52
Diana Flores García <diana_1310fg@hotmail.com>
2018-03-25 19:09:24 -0600

    Tratando de validar que las contraseñas coincidan..
    Tratando de validar que las contraseñas coincidan

--- SICUDE/registro.php
+++ SICUDE/registro.php
@@ -1,33 +1,38 @@
 <?php
+$matricula=$_POST['matricula'];
 $nombre=$_POST['nombre'];
-$appaterno=$_POST['appaterno'];
-$apmaterno=$_POST['apmaterno'];
-$direccion=$_POST['direccion'];
-$usuario=$_POST['usuario'];
-$pass=$_POST['psw'];
-$rpass=$_POST['rpsw'];
+$telefono=$_POST['telefono'];
+$correo=$_POST['correo'];
+$pass=$_POST['pswd'];
+$rpass=$_POST['pswdc'];
 
-if($pass==$rpass){
-	$matricula=$_POST['matricula'];
-	$pass=$_POST['pswd'];
-	$db_host="198.91.81.6";
-	$db_nombre="sicudex1_sistema";
-	$db_usuario="sicudex1_diana";
-	$db_pass="123456";
-	$conexion=mysqli_connect($db_host,$db_usuario,$db_pass,$db_nombre);
-	if(mysqli_connect_errno($db_nombre)){
-		echo mysqli_error($conexion);
+if(isset($_POST['terminos'])
+{
+	if($pass==$rpass){
+		$db_host="198.91.81.6";
+		$db_nombre="sicudex1_sistema";
+		$db_usuario="sicudex1_diana";
+		$db_pass="123456";
+		$conexion=mysqli_connect($db_host,$db_usuario,$db_pass,$db_nombre);
+		if(mysqli_connect_errno($db_nombre)){
+			echo mysqli_error($conexion);
+		}
+		else{
+			echo 'USTED HA SIDO REGISTRADO CORRECTAMENTE';
+			$registrar="INSERT INTO usuario VALUES('$matricula','$nombre','$telefono','$correo','$pass')";
+			mysqli_query($conexion,$registrar);
+			include("index.html");
+		}
+		mysqli_close($conexion);
 	}
 	else{
-		echo "DATOS REGISTRADOS";
-		$registrar="INSERT INTO persona VALUES('','$nombre','$appaterno','$apmaterno','$direccion','$usuario','$pass')";
-		mysqli_query($conexion,$registrar);
-		include("acceso.php");
+		echo "Las contraseñas no coinciden, inténtelo de nuevo.";
+		include("registro.html");
 	}
-	mysqli_close($conexion);
 }
-else{
-	echo "Las contraseñas no coinciden, intentelo de nuevo";
-	include("registro.php");
+else
+{
+	echo 'Necesita aceptar terminos y condiciones.';
 }
+
 ?>
